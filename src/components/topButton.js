a6831230544dfc2b355442748f9311acc856d023
Andre Ferreira <andrebmx789@gmail.com>
2021-01-30 17:05:23 -0300

    feat: create fixed top button

---
+++ src/components/topButton.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import { AiOutlineArrowUp } from 'react-icons/ai'
+import { AnchorLink } from "gatsby-plugin-anchor-links"
+
+const Button = styled.div`
+  position: fixed;
+  z-index: 1;
+  width: 50px;
+  height: 50px;
+  bottom: 30px;
+  right: 30px;
+`
+
+const Link = styled(AnchorLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  border-radius: 30px;
+  background: var(--button-color);
+  transition: .3s;
+
+  &:hover {
+    filter: opacity(60%);
+  }
+`
+
+export default function TopButton() {
+  return (
+    <Button>
+      <Link to="/#home"><AiOutlineArrowUp size="25"/></Link>
+    </Button>
+  )
+}
